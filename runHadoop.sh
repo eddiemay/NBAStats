92e9b389c5275a7452462ff231b424341af013b7
eddiemay <eddiemay1999@yahoo.com>
2018-01-28 00:11:15 -0800

    Setting of default requires login.

--- runHadoop.sh
+++ runHadoop.sh
@@ -1,3 +1,3 @@
 mvn -f hadoop.xml package
-rm -r target/output/fanduel/2018-01-26
+rm -r target/output/fanduel/
 ~/Downloads/hadoop-2.9.0/bin/hadoop jar target/NBAStats-hadoop-1.0.jar com.digitald4.nbastats.hadoop.ProcessFanDuel
