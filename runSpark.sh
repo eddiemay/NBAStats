12d22444dc74640eb56c3b88dad76098e38e7cc1
eddiemay <eddiemay1999@yahoo.com>
2018-02-08 09:27:32 -0800

    Setting of default requires login.

--- runSpark.sh
+++ runSpark.sh
@@ -1,7 +1,7 @@
 mvn -f distributed.xml package
 $SPARK_HOME/bin/spark-submit \
-  --class "com.digitald4.nbastats.distributed.spark.ProcessFanDuel" \
-  --master spark://poweredge410:7077 \
+  --class "com.digitald4.nbastats.distributed.spark.FantasyProcessor" \
+  --master local[*] \
   target/NBAStats-distributed-1.0.jar
 
   # --master local[*] \
