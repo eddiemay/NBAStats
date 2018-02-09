c828d55dfc74fe9e4140d33e2e8e4b2d0661fee6
eddiemay <eddiemay1999@yahoo.com>
2018-02-09 10:59:54 -0800

    Made spark the official way to mapreduce.

--- ProcessFanDuel.sh
+++ ProcessFanDuel.sh
@@ -1,3 +1,3 @@
 java -cp target/NBAStats-1.0/WEB-INF/lib/NBAStats-1.0.jar com.digitald4.nbastats.compute.FanDuelIO output
-sh runHadoop.sh
+sh runSpark.sh
 java -cp target/NBAStats-1.0/WEB-INF/lib/NBAStats-1.0.jar com.digitald4.nbastats.compute.FanDuelIO insert
