a3fc5b5f15adbcdd1c43fbab8ba4a0cf80a80fb3
eddiemay <eddiemay1999@yahoo.com>
2021-12-08 20:42:13 -0800

    Minor code change for resolveLogin

--- src/main/java/com/digitald4/nbastats/server/NBAStatsService.java
+++ src/main/java/com/digitald4/nbastats/server/NBAStatsService.java
@@ -1,11 +1,15 @@
 package com.digitald4.nbastats.server;
 
 import com.digitald4.common.server.service.EntityServiceImpl;
-import com.digitald4.common.storage.FakeLoginResolver;
 import com.digitald4.common.storage.Store;
 
 public class NBAStatsService<T> extends EntityServiceImpl<T> {
   public NBAStatsService(Store<T> store) {
-    super(store, new FakeLoginResolver(), false);
+    super(store, null, false);
+  }
+
+  @Override
+  protected void resolveLogin(String idToken, String method) {
+    // No login required for this api so do nothing.
   }
 }
