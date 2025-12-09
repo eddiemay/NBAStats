d0162ddfdef70ac96954e5d2f1db876d9edfaf3d
eddiemay <eddiemay1999@yahoo.com>
2025-12-09 08:35:11 -0800

    Improved fantasy calculation to not be dependent on weights being used for training. This will allow for weights to be hidden and discovered during training.

--- src/main/python/stats_fetcher/fantasy_calculator.py
+++ src/main/python/stats_fetcher/fantasy_calculator.py
@@ -5,38 +5,73 @@ from nba_stats_store import StatsStore
 from nba_player_store import PlayerStore
 
 
-fantasy_weights = { # FanDuel, DraftKings, NBA and NBA2017 Fantasy weights
+fantasy_weights_all = { # FanDuel, DraftKings, NBA and NBA2017 Fantasy weights
   'player_game_num_career': [0.0, 0.0, 0.0, 0.0],
   'team_game_num_season': [0.0, 0.0, 0.0, 0.0],
   'fg': [2.0, 2.0, 2.0, 2.0],
-  # 'fga': [0.0, 0.0, 0.0, 0.0],
-  # 'fg_pct': [0.0, 0.0, 0.0, 0.0],
+  'fga': [0.0, 0.0, 0.0, 0.0],
+  'fg_pct': [0.0, 0.0, 0.0, 0.0],
   'fg3': [1.0, 1.5, 1.0, 1.0],
-  # 'fg3a': [0.0, 0.0, 0.0, 0.0],
-  # 'fg3_pct': [0.0, 0.0, 0.0, 0.0],
-  # 'fg2': [0.0, 0.0, 0.0, 0.0],
-  # 'fg2a': [0.0, 0.0, 0.0, 0.0],
-  # 'fg2_pct': [0.0, 0.0, 0.0, 0.0],
-  # 'efg_pct': [0.0, 0.0, 0.0, 0.0],
+  'fg3a': [0.0, 0.0, 0.0, 0.0],
+  'fg3_pct': [0.0, 0.0, 0.0, 0.0],
+  'fg2': [0.0, 0.0, 0.0, 0.0],
+  'fg2a': [0.0, 0.0, 0.0, 0.0],
+  'fg2_pct': [0.0, 0.0, 0.0, 0.0],
+  'efg_pct': [0.0, 0.0, 0.0, 0.0],
   'ft': [1.0, 1.0, 1.0, 1.0],
-  # 'fta': [0.0, 0.0, 0.0, 0.0],
-  # 'ft_pct': [0.0, 0.0, 0.0, 0.0],
-  # 'orb': [0.0, 0.0, 0.0, 0.0],
-  # 'drb': [0.0, 0.0, 0.0, 0.0],
+  'fta': [0.0, 0.0, 0.0, 0.0],
+  'ft_pct': [0.0, 0.0, 0.0, 0.0],
+  'orb': [0.0, 0.0, 0.0, 0.0],
+  'drb': [0.0, 0.0, 0.0, 0.0],
   'trb': [1.2, 1.25, 1.0, 1.2],
   'ast': [1.5, 1.5, 2.0, 1.5],
   'stl': [3.0, 2.0, 3.0, 3.0],
   'blk': [3.0, 2.0, 3.0, 3.0],
   'tov': [-1.0, -0.5, -1.0, -1.0],
   'pf': [0.0, 0.0, 0.0, 0.0],
-  # 'pts': [0.0, 0.0, 0.0, 0.0],
+  'pts': [0.0, 0.0, 0.0, 0.0],
   'game_score': [0.0, 0.0, 0.0, 0.0],
   'plus_minus': [0.0, 0.0, 0.0, 0.0],
-  'double_double': [0.0, 1.5, 0.0, 0.0],
-  'triple_double': [0.0, 3.0, 0.0, 0.0],
+  'doubles': [0.0, 1.5, 0.0, 0.0],
+}
+fantasy_weights_complex = { # FanDuel, DraftKings, NBA and NBA2017 Fantasy weights
+  'player_game_num_career': [0.0, 0.0, 0.0, 0.0],
+  'team_game_num_season': [0.0, 0.0, 0.0, 0.0],
+  'fg': [2.0, 2.0, 2.0, 2.0],
+  'fg3': [1.0, 1.5, 1.0, 1.0],
+  'ft': [1.0, 1.0, 1.0, 1.0],
+  'trb': [1.2, 1.25, 1.0, 1.2],
+  'ast': [1.5, 1.5, 2.0, 1.5],
+  'stl': [3.0, 2.0, 3.0, 3.0],
+  'blk': [3.0, 2.0, 3.0, 3.0],
+  'tov': [-1.0, -0.5, -1.0, -1.0],
+  'pf': [0.0, 0.0, 0.0, 0.0],
+  'game_score': [0.0, 0.0, 0.0, 0.0],
+  'plus_minus': [0.0, 0.0, 0.0, 0.0],
+  'doubles': [0.0, 1.5, 0.0, 0.0],
+}
+fantasy_weights_simple = { # FanDuel, DraftKings, NBA and NBA2017 Fantasy weights
+  'fg3': [0.0, 0.5, 0.0, 0.0],
+  'trb': [1.2, 1.25, 1.0, 1.2],
+  'ast': [1.5, 1.5, 2.0, 1.5],
+  'stl': [3.0, 2.0, 3.0, 3.0],
+  'blk': [3.0, 2.0, 3.0, 3.0],
+  'tov': [-1.0, -0.5, -1.0, -1.0],
+  'pts': [1.0, 1.0, 1.0, 1.0],
+  'doubles': [0.0, 1.5, 0.0, 0.0],
+}
+fantasy_weights_no_doubles = { # FanDuel, DraftKings, NBA and NBA2017 Fantasy weights
+  'fg3': [0.0, 0.5, 0.0, 0.0],
+  'trb': [1.2, 1.25, 1.0, 1.2],
+  'ast': [1.5, 1.5, 2.0, 1.5],
+  'stl': [3.0, 2.0, 3.0, 3.0],
+  'blk': [3.0, 2.0, 3.0, 3.0],
+  'tov': [-1.0, -0.5, -1.0, -1.0],
+  'pts': [1.0, 1.0, 1.0, 1.0],
 }
 double_fields = ['pts', 'trb', 'ast', 'stl', 'blk']
 sample_idx = 959020
+fantasy_weights = fantasy_weights_simple
 
 
 def get_doubles(stat: dict):
@@ -50,15 +85,15 @@ def get_doubles(stat: dict):
 
 def set_doubles(stat: dict):
   doubles = get_doubles(stat)
-  stat['double_double'] = stat['triple_double'] = 0
+  stat['doubles'] = 0
   if doubles == 2:
-    stat['double_double'] = 1
+    stat['doubles'] = 1
   if doubles > 2:
-    stat['triple_double'] = 1
+    stat['doubles'] = 2
   return stat
 
 
-def to_numpy_array(stats: list):
+def to_numpy_array(stats: list, fantasy_weights = fantasy_weights):
   fan_values = []
   keys = list(fantasy_weights.keys())
   for stat in stats:
@@ -67,10 +102,13 @@ def to_numpy_array(stats: list):
     fan_values.append(value_list)
   return np.array(fan_values, dtype=np.float32)
 
-
-def calc_fantasy(npa):
+def matmul_fantasy(npa, fantasy_weights):
   weights = np.array(list(fantasy_weights.values()), dtype=np.float32)
   return np.matmul(npa, weights)
+
+def calc_fantasy(stats: list, fantasy_weights = fantasy_weights_simple):
+  npa = to_numpy_array(stats, fantasy_weights)
+  return matmul_fantasy(npa, fantasy_weights)
 
 if __name__ == '__main__':
   start_time = time.time()
@@ -89,14 +127,11 @@ if __name__ == '__main__':
 
   # Transform the data from dict array to numpy array
   npa = to_numpy_array(stats)
-  for fw in list(fantasy_weights.keys()):
-    for x in range(0):
-      fantasy_weights[fw].extend([1, 2, 3, 4, 5, 6, 7, 8, 10])
   print(npa[sample_idx])
   transform_time = time.time()
 
   # Perform the matrix multiplication to calculate the fantasy scores.
-  results = calc_fantasy(npa)
+  results = matmul_fantasy(npa, fantasy_weights)
   print(results[sample_idx])
   matmul_time = time.time()
 
@@ -117,5 +152,5 @@ if __name__ == '__main__':
         "\n\tLoad time:", load_time - start_time, "transform time:",
         transform_time - load_time, "matmul time:", mmt,
         'set & sort time:', end_time - matmul_time)
-  fpos = (len(npa) * len(list(fantasy_weights.values())) * len(fantasy_weights['fg']))
+  fpos = (len(npa) * len(list(fantasy_weights.values())) * len(fantasy_weights['fg3']))
   print("Billion Floating Point Operations:", fpos / 1000000000, "GFLOPs:", (1 / mmt * fpos) / 1000000000)
